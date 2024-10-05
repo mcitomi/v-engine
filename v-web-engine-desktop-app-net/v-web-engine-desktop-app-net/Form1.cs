4b27aeb8d92589c54e07980fa30eb2019f1a6da6
mcitomi <berdotamas5@gmail.com>
2024-10-05 18:28:06 +0200

    workin key detect

---
+++ v-web-engine-desktop-app-net/v-web-engine-desktop-app-net/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v_web_engine_desktop_app_net
+{
+    public partial class Form1 : Form
+    {
+        // win32 api stuffs
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _proc = HookCallback;
+            _hookID = SetHook(_proc);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                //string keyAction = wParam == (IntPtr)WM_KEYDOWN ? "pressed" : "released";
+                //string logEntry = $"Key {keyAction}: {((Keys)vkCode).ToString()}";
+
+                AddToLog(((Keys)vkCode).ToString());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private void AddToLog(string key)
+        {
+            // call listbox from main process if req
+            if (listBox1.InvokeRequired)
+            {
+                listBox1.Invoke(new Action(() => listBox1.Items.Insert(0, key)));
+            }
+            else
+            {
+                listBox1.Items.Insert(0, key);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+            base.OnFormClosing(e);
+        }
+    }
+}
+
